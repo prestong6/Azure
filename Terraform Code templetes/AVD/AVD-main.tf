7e39b021a551db7b00fa4855387352f3d86359a2
prestong6 <134627183+prestong6@users.noreply.github.com>
2023-06-15 13:45:13 -0400

    Update AVD-main.tf

--- Terraform Code templetes/AVD/AVD-main.tf
+++ Terraform Code templetes/AVD/AVD-main.tf
@@ -1,77 +1,90 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
+# Resource group name is output when execution plan is applied.
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.resource_group_location
+  tags = var.tags
+}
+
+resource "azurerm_virtual_network" "Vnet-AVD"{
+  name = "Vnet-AVD"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space = ["10.0.0.0/16"]
+  subnet {
+    name = "avd-subnet"
+    address_prefix = "10.0.3.0/24"
   }
 }
 
-provider "azurerm" {
-  features {}
+# Create AVD workspace
+resource "azurerm_virtual_desktop_workspace" "workspace" {
+  name                = var.workspace
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  friendly_name       = "${var.prefix} Workspace"
+  description         = "${var.prefix} Workspace"
+  tags = var.tags
 }
 
-#create an resource group 
-resource "azurerm_resource_group" "terraform" {
-    name = "terraformmain"
-    location = var.Azure_region
-}
-
-#Networking 
-resource "azurerm_virtual_network" "terraform-vnet" {
-    name = var.vnet
-    resource_group_name = azurerm_resource_group.terraform.name
-    location = var.Azure_region
-    address_space = ["10.0.0.0/16"]
-}
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.terraform.name
-  virtual_network_name = azurerm_virtual_network.terraform-vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            =  var.Azure_region
-  resource_group_name = azurerm_resource_group.terraform.name
-
-   ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-}
-}
-#Vm 
-
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = var.Azure_region
-  resource_group_name   = azurerm_resource_group.terraform.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-  delete_data_disks_on_termination = true
-
-   storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-datacenter-azure-edition-core"
-    version   = "latest"
+# Create AVD host pool
+resource "azurerm_virtual_desktop_host_pool" "hostpool" {
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  name                     = var.hostpool
+  friendly_name            = var.hostpool
+  validate_environment     = true #[true false]
+  start_vm_on_connect      = true
+  custom_rdp_properties    = "targetisaadjoined:i:1;drivestoredirect:s:*;audiomode:i:0;videoplaybackmode:i:1;redirectclipboard:i:1;redirectprinters:i:1;devicestoredirect:s:*;redirectcomports:i:1;redirectsmartcards:i:1;usbdevicestoredirect:s:*;enablecredsspsupport:i:1;redirectwebauthn:i:1;use multimon:i:1;enablerdsaadauth:i:1;"
+  description              = "${var.prefix} HostPool"
+  type                     = "Personal" #[Pooled or Personal]
+  personal_desktop_assignment_type = "Automatic"
+  load_balancer_type       =  "Persistent"
+  tags = var.tags
+scheduled_agent_updates {
+  enabled = true
+  timezone = "Eastern Standard Time"  # Update this value with your desired timezone
+  schedule {
+    day_of_week = "Saturday"
+    hour_of_day = 1   #[1 here means 1:00 am]
   }
-  storage_os_disk {
-    name              = "disk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "notadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_windows_config {
-    provision_vm_agent = true
-    }
-
 }
+}
+
+resource "azurerm_virtual_desktop_host_pool_registration_info" "registrationinfo" {
+  hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
+  expiration_date = var.rfc3339
+}
+
+# Create AVD DAG
+resource "azurerm_virtual_desktop_application_group" "dag" {
+  resource_group_name = azurerm_resource_group.rg.name
+  host_pool_id        = azurerm_virtual_desktop_host_pool.hostpool.id
+  location            = azurerm_resource_group.rg.location
+  type                = "Desktop"
+  name                = var.app_group_name
+  friendly_name       = "Desktop AppGroup"
+  description         = "${var.prefix} AVD application group"
+  depends_on          = [azurerm_virtual_desktop_host_pool.hostpool, azurerm_virtual_desktop_workspace.workspace]
+  tags = var.tags
+}
+
+# Associate Workspace and DAG
+resource "azurerm_virtual_desktop_workspace_application_group_association" "ws-dag" {
+  application_group_id = azurerm_virtual_desktop_application_group.dag.id
+  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+}
+
+# Assign AAD Group to the Desktop Application Group (DAG)
+resource "azurerm_role_assignment" "AVDGroupDesktopAssignment" {
+  scope                = azurerm_virtual_desktop_application_group.dag.id
+  role_definition_name = "Desktop Virtualization User"
+  principal_id         = data.azuread_group.AVDGroup.object_id
+}
+
+# Assign AAD Group to the Resource Group for RBAC for the Session Host
+resource "azurerm_role_assignment" "RBACAssignment" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Virtual Machine User Login"
+  principal_id         = data.azuread_group.AVDGroup.object_id
+}
+
