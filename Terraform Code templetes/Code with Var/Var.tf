b9df7a264536b1369e6a7a4f30ade6aa3a2b75fb
prestong6 <134627183+prestong6@users.noreply.github.com>
2023-06-15 08:24:16 -0400

    Update Var.tf

--- Terraform Code templetes/Code with Var/Var.tf
+++ Terraform Code templetes/Code with Var/Var.tf
@@ -2,3 +2,15 @@ variable "Azure_region" {
   type = string
   default = "West US"
 }
+variable "prefix" {
+  default = "TestVM"
+}
+variable "vnet" {
+  type = string 
+  default = "terraform-vnet"
+}
+variable "ARG" {
+  type = string
+  default = "terraform"
+  
+}
